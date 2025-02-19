afdad0e01b19e65083bf45331fefaed6cceb8157
mrussel3 <mrussel3@fandm.edu>
2025-02-19 13:23:57 -0500

    Second Commit, Added tests and comments, need to Finalize

--- TimeCode.cpp
+++ TimeCode.cpp
@@ -1,69 +1,91 @@
-#include <iostream>
-#include <sstream>
-#include <iomanip>
+#include <iostream> //provides cout
+#include <sstream> // provides stringstream
+
 using namespace std;
 
-#include "TimeCode.h"
+//AUTHOR: MAX RUSSELL
 
+#include "TimeCode.h"  // include header file
+
+//implement constructor, set t = to seconds for given hr, min, sec
 TimeCode::TimeCode(unsigned int hr, unsigned int min, long long unsigned int sec){
     t = ComponentsToSeconds(hr, min, sec);
 }
 
+//set t = to given TimeCode
 TimeCode::TimeCode(const TimeCode& tc){
     t = tc.t;
 }
 
+//set the hours equal to value that is taken in and ensure is non-negative
 void TimeCode::SetHours(unsigned int hours){
-    unsigned int min, sec;
-    GetComponents(hours, min, sec);
+    unsigned int hr, min, sec;
+    GetComponents(hr, min, sec);
     t = ComponentsToSeconds(hours, min, sec);
 }
 
+//set the minutes equal to the value that is taken in, ensure minutes is < 60 and not negative
 void TimeCode::SetMinutes(unsigned int minutes){
-    unsigned int hr, sec;
-    GetComponents(hr, minutes, sec);
+    unsigned int hr, min, sec;
+
+    if (minutes >= 60){
+        throw std::invalid_argument("Minutes must be less than 60: "+ to_string(minutes)); // throws error when minutes is >= 60
+    }
+
+    GetComponents(hr, min, sec);
     t = ComponentsToSeconds(hr, minutes, sec);
 }
-
+//set the seconds equal to the value that is taken in, ensure minutes is < 60 and not negative
 void TimeCode::SetSeconds(unsigned int seconds){
-    unsigned int hr, min;
-    GetComponents(hr, min, seconds);
+    unsigned int hr, min, sec;
+
+    if (seconds >= 60){
+        throw std::invalid_argument("Seconds must be less than 60: " + to_string(seconds)); // throws error when seconds is >= 60
+    }
+    GetComponents(hr, min, sec);
     t = ComponentsToSeconds(hr, min, seconds);
 }
 
+//resets TimeCode
 void TimeCode::reset(){
     t = 0;
 }
 
+//Returns the hours using GetComponents
 unsigned int TimeCode::GetHours() const{
     unsigned int hr, min, sec;
     GetComponents(hr, min, sec);
     return hr;
 }
 
+//Returns the minutes using GetComponents
 unsigned int TimeCode::GetMinutes() const{
     unsigned int hr, min, sec;
     GetComponents(hr, min, sec);
     return min;
 }
+
+//Returns the seconds using GetComponents
 unsigned int TimeCode::GetSeconds() const{
     unsigned int hr, min, sec;
     GetComponents(hr, min, sec);
     return sec;
 }
 
+//GetComponents uses pass by reference to take in hr, min, sec and assign these variables with their correct number
 void TimeCode::GetComponents(unsigned int& hr, unsigned int& min, unsigned int& sec) const{
     hr = t/3600;
     min = (t%3600) /60;
     sec = t % 60;
 }
 
+//Converts input hr, min, sec to seconds 
 long long unsigned int TimeCode::ComponentsToSeconds(unsigned int hr, unsigned int min, unsigned long long int sec){
     return (hr*3600) + (min*60) + sec;
 }
 
 //Resource for learning to use stringstream : https://www.geeksforgeeks.org/stringstream-c-applications/ 
-
+//Prints TimeCode in " 0:0:0 " format
 string TimeCode::ToString() const{
     unsigned int hr, min, sec;
     GetComponents(hr, min, sec);
@@ -82,10 +104,12 @@ string TimeCode::ToString() const{
     return h + ":" + m + ":" + s;
 }
 
+//overloads addition operator, allowing addition of two time codes
 TimeCode TimeCode::operator+(const TimeCode& other) const{
     return TimeCode(0, 0, t + other.t);
 }
 
+//overloads subtraction operator, allowing subtraction of two time codes
 TimeCode TimeCode::operator-(const TimeCode& other) const{
     if (t < other.t){
         throw std::invalid_argument("Resulting TimeCode cannot be negative.");
@@ -93,6 +117,7 @@ TimeCode TimeCode::operator-(const TimeCode& other) const{
     return TimeCode(0, 0, t - other.t);
 }
 
+//overloads multiplication operator, allowing multiplication of a time code and a double
 TimeCode TimeCode::operator*(double a) const{
     if (a<0){
         throw std::invalid_argument("Double cannot be negative.");
@@ -100,6 +125,7 @@ TimeCode TimeCode::operator*(double a) const{
     return TimeCode(0, 0, static_cast<unsigned long long int>(t * a));
 }
 
+//overloads division operator, allowing division of a time code and a double
 TimeCode TimeCode::operator/(double a) const{
     if (a <=0){
         throw std::invalid_argument("Double cannot be negative or 0.");
@@ -107,26 +133,32 @@ TimeCode TimeCode::operator/(double a) const{
     return TimeCode(0, 0, static_cast<unsigned long long int>(t / a));
 }
 
+// overloads == operator, allowing us to see if two TimeCodes are equal
 bool TimeCode::operator == (const TimeCode& other) const{
     return t == other.t;
 }
 
+// overloads != operator, allowing us to see if two TimeCodes are not equal
 bool TimeCode::operator != (const TimeCode& other) const{
     return t != other.t;
 }
 
+// overloads < operator, allowing us to see if our TimeCode is less than the other TimeCode
 bool TimeCode::operator < (const TimeCode& other) const{
     return t < other.t;
 }
 
+// overloads <= operator, allowing us to see if our TimeCode is less than or equal to the other TimeCode
 bool TimeCode::operator <= (const TimeCode& other) const{
     return t <= other.t;
 }
 
+// overloads > operator, allowing us to see if our TimeCode is greater than the other TimeCode
 bool TimeCode::operator > (const TimeCode& other) const{
     return t > other.t;
 }
 
+// overloads >= operator, allowing us to see if our TimeCode is greater than or equal to the other TimeCode
 bool TimeCode::operator >= (const TimeCode& other) const{
     return t >= other.t;
 }
--- TimeCodeTests.cpp
+++ TimeCodeTests.cpp
@@ -12,7 +12,12 @@ void TestComponentsToSeconds(){
 	// Random but "safe" inputs
 	long long unsigned int t = TimeCode::ComponentsToSeconds(3, 17, 42);
 	assert(t == 11862);
+
+	t = TimeCode::ComponentsToSeconds(3, 17, 289);
+	assert(t == 12109);
 	
+	t = TimeCode::ComponentsToSeconds(3, 62, 42);
+	assert(t == 14562);
 	// More tests go here!
 	
 	cout << "PASSED!" << endl << endl;
@@ -38,14 +43,19 @@ void TestComponentConstructor(){
 	//cout << "tc: " << tc.ToString() << endl;
 	assert(tc.ToString() == "0:0:0");
 	
-	// more tests go here!
+	tc = TimeCode(3, 14, 25);
+	assert(tc.ToString() == "3:14:25");
+
+	cout << "Testing Reset" << endl;
+	tc.reset();
+	assert(tc.ToString() == "0:0:0");
 	
 	// Roll-over inputs
 	TimeCode tc3 = TimeCode(3, 71, 3801);
 	//cout << "tc3: " << tc3.ToString() << endl;
 	assert(tc3.ToString() == "5:14:21");
 	
-	// More tests go here!
+	
 	
 	cout << "PASSED!" << endl << endl;
 }
@@ -63,20 +73,29 @@ void TestGetComponents(){
 	tc.GetComponents(h, m, s);
 	assert(h == 5 && m == 2 && s == 18);
 	
-	// More tests go here!
+	tc = TimeCode(10, 6, 29);
+	tc.GetComponents(h, m, s);
+	assert(h == 10 && m == 6 && s == 29);
 	
+	cout << "PASSED!" << endl << endl;
+
+	cout << "Testing Get functions" << endl;
+	tc = TimeCode(15, 8, 45);
+	assert(tc.GetHours() == 15);
+	assert(tc.GetMinutes() == 8);
+	assert(tc.GetSeconds() == 45);
+
 	cout << "PASSED!" << endl << endl;
 }
 
 
-void TestSubtract(){
+void TestOperators(){
+
 	cout << "Testing Subtract" << endl;
 	TimeCode tc1 = TimeCode(1, 0, 0);
 	TimeCode tc2 = TimeCode(0, 50, 0);
 	TimeCode tc3 = tc1 - tc2;
 	assert(tc3.ToString() == "0:10:0");
-	
-	
 	TimeCode tc4 = TimeCode(1, 15, 45);
 	try{
 		TimeCode tc5 = tc1 - tc4;
@@ -87,38 +106,126 @@ void TestSubtract(){
 		// just leave this empty
 		// and keep doing more tests
 	}
+	cout << "Testing Addition" << endl;
+	tc1 = TimeCode(1, 0, 3);
+	tc2 = TimeCode(0, 50, 48);
+	tc3 = tc1 + tc2;
+	assert(tc3.ToString()=="1:50:51");
 
-	// more tests
+	tc1 = TimeCode(1, 15, 17);
+	tc2 = TimeCode(0, 50, 48);
+	tc3 = tc1 + tc2;
+	assert(tc3.ToString()=="2:6:5");
+
+	cout << "Testing Multiplication" << endl;
+	tc1 = TimeCode(1,30,0);
+	tc2 = tc1 * 0.5;
+	assert(tc2.ToString()=="0:45:0");
+
 	
+	tc1 = TimeCode(2,10,15);
+	tc2 = tc1 * 2;
+	assert(tc2.ToString()=="4:20:30");
+
+	cout << "Testing Division" << endl;
+	tc1 = TimeCode(6,18, 42);
+	tc2 = tc1 / 6 ; 
+	assert(tc2.ToString()== "1:3:7");
+	cout << "PASSED!" << endl << endl;
+
+	cout <<"Testing Comparison Operators" << endl;
+	tc1 = TimeCode(6,18, 42);
+	tc2 = TimeCode(6, 18, 42);
+	bool result = tc1 == tc2;
+	assert(result == true);
+
+	tc1 = TimeCode(6,18, 42);
+	tc2 = TimeCode(5, 30, 42);
+	result = tc1 == tc2;
+	assert(result == false);
+
+	tc1 = TimeCode(6,18, 42);
+	tc2 = TimeCode(5, 30, 42);
+	result = tc1 != tc2;
+	assert(result == true);
+
+	tc1 = TimeCode(6,18, 42);
+	tc2 = TimeCode(6, 18, 42);
+	result = tc1 != tc2;
+	assert(result == false);
+
+	tc1 = TimeCode(6,18, 42);
+	tc2 = TimeCode(7, 20, 50);
+	result = tc1 < tc2;
+	assert(result == true);
+
+	tc2 = TimeCode(6,18, 42);
+	tc1 = TimeCode(7, 20, 50);
+	result = tc1 < tc2;
+	assert(result == false);
+
+	tc1 = TimeCode(6,18, 42);
+	tc2 = TimeCode(6, 18, 42);
+	result = tc1 <= tc2;
+	assert(result == true);
+
+	tc2 = TimeCode(6,18, 42);
+	tc1 = TimeCode(7, 20, 50);
+	result = tc1 > tc2;
+	assert(result == true);
+
+	tc1 = TimeCode(6,18, 42);
+	tc2 = TimeCode(7, 20, 50);
+	result = tc1 > tc2;
+	assert(result == false);
+
+	tc1 = TimeCode(6,18, 42);
+	tc2 = TimeCode(6, 18, 42);
+	result = tc1 >= tc2;
+	assert(result == true);
+
 	cout << "PASSED!" << endl << endl;
 }
 
 
-void TestSetMinutes()
+void TestSet()
 {
 	cout << "Testing SetMinutes" << endl;
-
 	TimeCode tc = TimeCode(8, 5, 9);
 	tc.SetMinutes(15); // test valid change
 	assert(tc.ToString() == "8:15:9");
-
 	try
 	{
 		tc.SetMinutes(80);  // test invalid change
 		assert(false);
 	}
 	catch (const invalid_argument &e)
+	{   }
+	cout << "PASSED!" << endl << endl;
+
+	cout << "Testing SetHours" << endl;
+	tc = TimeCode(8, 5, 9);
+	tc.SetHours(15); // test valid change
+	assert(tc.ToString() == "15:5:9");
+	
+	cout << "PASSED!" << endl << endl;
+
+	cout << "Testing SetSeconds" << endl;
+	tc = TimeCode(8, 5, 9);
+	tc.SetSeconds(15); // test valid change
+	assert(tc.ToString() == "8:5:15");
+	try
 	{
-		// cout << e.what() << endl;
+		tc.SetSeconds(60);  // test invalid change
+		assert(false);
 	}
-
-	assert(tc.ToString() == "8:15:9");
-
+	catch (const invalid_argument &e)
+	{   }
 	cout << "PASSED!" << endl << endl;
 }
 
 
-// Many More Tests...
+
 
 	
 int main(){
@@ -127,10 +234,12 @@ int main(){
 	TestDefaultConstructor();
 	TestComponentConstructor();
 	TestGetComponents();
+	TestOperators();
+	TestSet();
 	
 	// Many othere test functions...
 	
 	cout << "PASSED ALL TESTS!!!" << endl;
-	cout << "YESSS" << endl;
+	
 	return 0;
 }
